b20b4b995d9a3e61ac6bb9eb3d5c92a5b3b198db
David Davó <ddavo@ucm.es>
2020-02-21 15:39:00 +0100

    Ejemplo 2 terminado

--- 0 Ejemplos/Ejemplo2/main.cu
+++ 0 Ejemplos/Ejemplo2/main.cu
@@ -2,8 +2,19 @@
 #include <malloc.h>
 #include <stdlib.h>
 #include <math.h>
+#include <sys/time.h>
 
 #include <cuda.h>
+
+double wtime(void)
+{
+        static struct timeval   tv0;
+        double time_;
+
+        gettimeofday(&tv0,(struct timezone*)0);
+        time_=(double)((tv0.tv_usec + (tv0.tv_sec)*1000000));
+        return( time_/1000000);
+}
 
 
 void Mul(float *A, float *B, int hA, int wA, int wB, float *C)
@@ -18,15 +29,14 @@ void Mul(float *A, float *B, int hA, int wA, int wB, float *C)
 		}
 }
 
-__global__ void cudaMul(float * A, float * B, int ha, int wa, int wB, float * C) {
-	int i = blockIdx.x * blockDim.x + threadIdx.x;
+__global__ void cudaMul(float * A, float * B, int ha, int wa, int wb, float * C) {
+	int i = blockIdx.y * blockDim.y + threadIdx.y;
+	int j = blockIdx.x * blockDim.x + threadIdx.x;
 
-	if (i < ha)
-		for (int j = 0; j < wB; j++)
-			for (int k = 0; k < wa; k++)
-				C[i*wB+j] += A[i*wa+k]*B[k*wB+j];
+	if (i < ha && j < wb)
+		for (int k = 0; k < wa; k++)
+			C[i*wb+j] += A[i*wa+k]*B[k*wb+j];
 }
-
 
 void init_matrix(float *M, int hM, int wM, float k)
 {
@@ -60,6 +70,8 @@ int diff(float *A, float *B, int hA, int wA, int wB, float *C)
 
 	int i,j,k;
 
+	double t0, t1;
+	t0 = wtime();
 	for (i=0; i<hA; i++)
 		for (j=0; j<wB; j++){
 			C_cpu[i*wB+j] = 0.0;
@@ -67,6 +79,8 @@ int diff(float *A, float *B, int hA, int wA, int wB, float *C)
 				C_cpu[i*wB+j] += A[i*wA+k]*B[k*wB+j];
 			}
 		}
+	t1 = wtime();
+	printf("Time CPU: %f\n", t1-t0);
 	//printf("\n\nMATRIX C_cpu\n");print_matrix(C_cpu, hA, wB);
 
 	for (i=0; i<hA; i++)
@@ -123,30 +137,40 @@ int main(int argc, char** argv)
 
 	cudaMemcpy(A_GPU, A, size_A*sizeof(float), cudaMemcpyHostToDevice);
 	cudaMemcpy(B_GPU, B, size_B*sizeof(float), cudaMemcpyHostToDevice);
-	cudaMemset(C_GPU, 0, size_C*sizeof(float));
+	cudaMemset(C_GPU, 0.0f, size_C*sizeof(float));
 
 	C = (float*)malloc(size_C*sizeof(float));
 
-	Mul(A, B, hA, wA, wB, C);
-	printf("\n\nMATRIX A\n");print_matrix(A, hA, wA);
-	printf("\n\nMATRIX B\n");print_matrix(B, hB, wB);
-	printf("\n\nMATRIX C\n");print_matrix(C, hA, wB);
+	// Mul(A, B, hA, wA, wB, C);
+	// printf("\n\nMATRIX A\n");print_matrix(A, hA, wA);
+	// printf("\n\nMATRIX B\n");print_matrix(B, hB, wB);
+	// printf("\n\nMATRIX C\n");print_matrix(C, hA, wB);
 
+	#define THX 32
+	#define THY 32
 	// Ahora sí que hacemos el calculo
-	dim3 b(1024, 1);
-	dim3 g(ceil(size_C/1024.0), 1);
+	dim3 b(THX, THY); // Threads por bloque
+	dim3 g(ceil(float(hA)/float(b.x)), ceil(float(wB)/float(b.y))); // Bloques por grid
+	printf("C_SIZE: %d x %d (%d), b: %d x %d, g: %d x %d\n", wB, hA, size_C, b.x, b.y, g.x, g.y);
+	double t0, t1;
+
+	t0 = wtime();
 	cudaMul<<<g, b>>>(A_GPU, B_GPU, hA, wA, wB, C_GPU);
+	cudaDeviceSynchronize();
+	t1 = wtime();
+	printf("Time GPU: %f\n", t1-t0);
 
 	cudaMemcpy(C, C_GPU, size_C*sizeof(float), cudaMemcpyDeviceToHost);
 
 	if (!diff(A, B, hA, wA, wB, C))
 		printf("ERROR=GPU.vs.CPU matrix mult differs\n");
-	
+	else
+		printf("Everything went fine\n");
 
 	// print Matrix
 	// printf("\n\nMATRIX A\n");print_matrix(A, hA, wA);
 	// printf("\n\nMATRIX B\n");print_matrix(B, hB, wB);
-	printf("\n\nMATRIX C\n");print_matrix(C, hA, wB);
+	// printf("\n\nMATRIX C\n");print_matrix(C, hA, wB);
 
 	free(A); free(B); free(C);
 
