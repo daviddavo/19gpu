781e20c5216964472ce33f74f7566a7c858ba6ba
David Davó <david@ddavo.me>
2020-04-15 17:42:59 +0200

    Pues eso

--- 0 Ejemplos/Ejemplo3/compilar.sh
+++ 0 Ejemplos/Ejemplo3/compilar.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 nvcc -c matrix_mul.cu -I/usr/local/cuda/NVIDIA_GPU_Computing_SDK/C/common/inc/ -L/usr/local/cuda/lib64/
-g++ -o matrix matrix_mul.o main.c -I./ -I/usr/local/cuda/NVIDIA_GPU_Computing_SDK/C/common/inc/ -L/usr/local/cuda/lib64/ -lcuda -lcudart
+# g++ -o matrix matrix_mul.o main.c -I./ -I/usr/local/cuda/NVIDIA_GPU_Computing_SDK/C/common/inc/ -L/usr/local/cuda/lib64/ -lcuda -lcudart
+g++ -o matrix matrix_mul.o main.c -I./ -I/usr/local/cuda/NVIDIA_GPU_Computing_SDK/C/common/inc/ -L/opt/cuda/targets/x86_64-linux/lib/ -lcuda -lcudart
